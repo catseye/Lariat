0430ad8c4ba48579e5062a506518738f420a0b67
Chris Pressey <cpressey@catseye.tc>
2022-05-01 21:49:18 +0100

    Begin updating the Haskell implementation to match what's in 0.2.

--- impl/Haskell/Data/Lariat.hs
+++ impl/Haskell/Data/Lariat.hs
@@ -1,43 +1,54 @@
-module Data.Lariat (var, app, abs, resolve, destruct, freevars) where
+module Data.Lariat (var, app, abs, destruct) where
 
 import Prelude hiding (abs)
-import Data.List (nub)
 
-data Term α = FreeVar α
-            | App (Term α) (Term α)
-            | Abs (Term α)
-            | BoundVar Integer
+-- For now, our Term type is non-polymorphic, and
+-- always uses this Name type.
+
+type Name = [String]
+
+data Term = FreeVar Name
+          | App Term Term
+          | Abs Term
+          | BoundVar Integer
     deriving (Show, Ord, Eq)
 
-var :: α -> Term α
+var :: Name -> Term
 var n = FreeVar n
 
-app :: Term α -> Term α -> Term α
+app :: Term -> Term -> Term
 app t u = App t u
 
-abs :: Eq α => α -> Term α -> Term α
+abs :: Name -> Term -> Term
 abs n t = Abs (bind n t 0) where
     bind n (App t u) level = App (bind n t level) (bind n u level)
     bind n (Abs t) level = Abs (bind n t (level + 1))
     bind n t@(FreeVar m) level = if n == m then (BoundVar level) else t
     bind _ t _ = t
 
-resolve :: Term α -> Term α -> Term α
-resolve (Abs t) u = unbind t u 0 where
-    unbind (App t u) x level = App (unbind t x level) (unbind u x level)
-    unbind (Abs t) x level = Abs (unbind t x (level + 1))
-    unbind t@(BoundVar i) x level = if i == level then x else t
-    unbind t _ _ = t
-resolve t u = t
-
-destruct :: Term α -> (α -> β) -> (Term α -> Term α -> β) -> (Term α -> β) -> β
+destruct :: Term -> (Name -> β) -> (Term -> Term -> β) -> (Term -> Name -> β) -> β
 destruct (FreeVar n) f _ _ = f n
 destruct (App t u)   _ f _ = f t u
-destruct t@(Abs _)   _ _ f = f t
+destruct (Abs b)     _ _ f =
+    let
+        vars = freeVars b
+        n = newName vars
+        u = unbind b (var n) 0
+    in
+        f u n
+    where
+        freeVars (FreeVar n) = [n]
+        freeVars (App t u) = (freeVars t) ++ (freeVars u)
+        freeVars (Abs b) = (freeVars b)
+        freeVars _ = []
 
-freevars :: Eq α => Term α -> [α]
-freevars t = nub (freevars' t) where
-    freevars' (FreeVar n) = [n]
-    freevars' (App t u) = (freevars' t) ++ (freevars' u)
-    freevars' (Abs t) = freevars' t
-    freevars' _ = []
+        newName vars = expand $ longestName vars []
+        expand [] = ["y"]
+        expand (x:xs) = x:(x:xs)
+        longestName [] acc = acc
+        longestName (x:xs) acc = longestName xs (if (length x) > (length acc) then x else acc)
+
+        unbind (App t u) x level = App (unbind t x level) (unbind u x level)
+        unbind (Abs t) x level = Abs (unbind t x (level + 1))
+        unbind t@(BoundVar i) x level = if i == level then x else t
+        unbind t _ _ = t
--- impl/Haskell/LariatDemo.hs
+++ impl/Haskell/LariatDemo.hs
@@ -2,138 +2,36 @@ module LariatDemo where
 
 import Prelude hiding (abs)
 import Data.Char (ord, chr)
-import Data.Lariat (var, app, abs, resolve, destruct, freevars)
+import Data.List (intercalate)
+import Data.Lariat (var, app, abs, destruct)
 
 --
 -- Test cases
 -- (should really be unit tests; for now, run them manually)
 --
 
-testVar = (var "n")
-testAbs = (abs "n" (var "n"))
+n = ["n"]
+m = ["m"]
+
+showName n = intercalate "." n
+
+testVar = (var n)
+testAbs = (abs n (var n))
 testApp = app testAbs testVar
 
 testDestruct t = destruct t
-                    (\n   -> "var:" ++ (show n))
+                    (\n   -> "var:" ++ (showName n))
                     (\s t -> "app:" ++ (show s) ++ "," ++ (show t))
-                    (\t   -> "abs:" ++ (show t))
+                    (\t n -> "abs:" ++ (showName n) ++ ":" ++ (show t))
 
-test2a = testDestruct (var "n")
-test2b = testDestruct (app (var "n") (var "m"))
-test2c = testDestruct (abs "n" (var "n"))
+test2a = testDestruct (var n)
+test2b = testDestruct (app (var n) (var m))
+test2c = testDestruct (abs n (var n))
 
-test3a = (freevars testApp)                     == ["n"]
-test3b = (freevars testAbs)                     == []
-test3c = (freevars (app (var "n") (var "n")))   == ["n"]
+--test3a = (freevars testApp)                     == ["n"]
+--test3b = (freevars testAbs)                     == []
+--test3c = (freevars (app (var "n") (var "n")))   == ["n"]
 
-test4a = (show $ resolve (testApp) (var "q"))               == "App (Abs (BoundVar 0)) (FreeVar \"n\")"
-test4b = (show $ resolve (abs "n" (var "m")) (var "q"))     == "FreeVar \"m\""
-test4c = (show $ resolve (abs "n" (var "n")) (var "q"))     == "FreeVar \"q\""
-
---
--- Name supply
---
-
-intToName n =
-    let
-        p = if n > 26 then (intToName (n `div` 26)) else ""
-        n' = n `mod` 26
-        c = chr (ord 'a' + n')
-    in
-        p ++ [c]
-
-names = map (intToName) [0..]
-
-pick exclude names =
-    let
-        [n] = take 1 names
-        names' = drop 1 names
-        excluded = elem n exclude
-    in
-        if excluded then pick exclude names' else (n, names')
-
---
--- "Contains a free variable named _j_" example
---
-
-contains j t names =
-    destruct t
-        (\n   -> n == j)
-        (\u v -> contains j u names || contains j v names)
-        (\u   ->
-            let
-                (n, names') = pick [j] names
-                t' = resolve t (var n)
-            in
-                contains j t' names'
-        )
-
-testContains1 = (contains "n" testApp names)                 == True
-testContains2 = (contains "m" testApp names)                 == False
-testContains3 = (contains "n" (abs "n" (var "n")) names)     == False
-
---
--- "beta-reduce a term" example
---
-
-beta t = destruct t
-    (\n   -> var n)
-    (\u v -> destruct u
-        (\_   -> app u v)
-        (\_ _ -> app u v)
-        (\u   -> resolve u v)
-    )
-    (\u   -> u)
-
-testBeta1 = (show $ beta testApp)                    == "FreeVar \"n\""
-testBeta2 = (show $ beta testAbs)                    == "Abs (BoundVar 0)"
-testBeta3 = (show $ beta (var "j"))                  == "FreeVar \"j\""
-
-isBetaReducible t = destruct t
-    (\n   -> False)
-    (\u v -> destruct u
-        (\_   -> False)
-        (\_ _ -> False)
-        (\u   -> True)
-    )
-    (\u   -> False)
-
-testIsBeta1 = (isBetaReducible testApp)       == True
-testIsBeta2 = (isBetaReducible testAbs)       == False
-testIsBeta3 = (isBetaReducible (var "j"))     == False
-
-reduceOnce t =
-    if isBetaReducible t then (Right (beta t)) else destruct t
-        (\n   -> Left (var n))
-        (\u v ->
-            case reduceOnce u of
-                Right r -> Right (app r v)
-                Left f  ->
-                    case reduceOnce v of
-                        Right s -> Right (app u s)
-                        Left  g -> Left (app f g)
-        )
-        (\u   -> Left u)
-
-testReduceOnce1 = (show $ reduceOnce testApp)          == "Right (FreeVar \"n\")"
-testReduceOnce2 = (show $ reduceOnce testAbs)          == "Left (Abs (BoundVar 0))"
-testReduceOnce3 = (show $ reduceOnce (var "j"))        == "Left (FreeVar \"j\")"
-
-testReduceOnce4 =
-    let
-       t = (app (app (abs "j" (var "j")) (abs "j" (var "j"))) (var "n"))
-    in
-       (show $ reduceOnce t) == "Right (App (Abs (BoundVar 0)) (FreeVar \"n\"))"
-
-testReduceOnce5 =
-    let
-       t = (app (app (abs "j" (var "j")) (abs "j" (var "j"))) (var "n"))
-       (Right t') = reduceOnce t
-    in
-       (show $ reduceOnce t') == "Right (FreeVar \"n\")"
-
-testReduceOnce6 =
-    let
-       t = (app (var "k") (app (abs "j" (var "j")) (var "m")))
-    in
-       (show $ reduceOnce t) == "Right (App (FreeVar \"k\") (FreeVar \"m\"))"
+--test4a = (show $ resolve (testApp) (var "q"))               == "App (Abs (BoundVar 0)) (FreeVar \"n\")"
+--test4b = (show $ resolve (abs "n" (var "m")) (var "q"))     == "FreeVar \"m\""
+--test4c = (show $ resolve (abs "n" (var "n")) (var "q"))     == "FreeVar \"q\""
