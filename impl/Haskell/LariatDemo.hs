84b486a7005c778d98df76bc7bc3ed3edb6ef022
Chris Pressey <cpressey@catseye.tc>
2022-05-02 08:51:44 +0100

    Implement `freevars` using the abstraction and test it.

--- impl/Haskell/LariatDemo.hs
+++ impl/Haskell/LariatDemo.hs
@@ -2,7 +2,7 @@ module LariatDemo where
 
 import Prelude hiding (abs)
 import Data.Char (ord, chr)
-import Data.List (intercalate)
+import Data.List (intercalate, nub)
 import Data.Lariat (var, app, abs, destruct)
 
 --
@@ -13,24 +13,28 @@ import Data.Lariat (var, app, abs, destruct)
 n = ["n"]
 m = ["m"]
 
-showName n = intercalate "." n
-
 testVar = (var n)
 testAbs = (abs n (var n))
 testApp = app testAbs testVar
 
 testDestruct t = destruct t
-                    (\n   -> "var:" ++ (showName n))
-                    (\s t -> "app:" ++ (show s) ++ "," ++ (show t))
-                    (\t n -> "abs:" ++ (showName n) ++ ":" ++ (show t))
+                    (\n   -> "var:" ++ (show n))
+                    (\u v -> "app:" ++ (show u) ++ "," ++ (show v))
+                    (\u n -> "abs:" ++ (show n) ++ ":" ++ (show u))
 
 test2a = testDestruct (var n)
 test2b = testDestruct (app (var n) (var m))
 test2c = testDestruct (abs n (var n))
 
---test3a = (freevars testApp)                     == ["n"]
---test3b = (freevars testAbs)                     == []
---test3c = (freevars (app (var "n") (var "n")))   == ["n"]
+freevars t = nub $ freevars' t [] where
+    freevars' t ours = destruct t
+                        (\n   -> if elem n ours then [] else [n])
+                        (\u v -> freevars' u ours ++ freevars' v ours)
+                        (\u n -> freevars' u (n:ours))
+
+test3a = (freevars testApp)               == [n]
+test3b = (freevars testAbs)               == []
+test3c = (freevars (app (var n) (var n))) == [n]
 
 --test4a = (show $ resolve (testApp) (var "q"))               == "App (Abs (BoundVar 0)) (FreeVar \"n\")"
 --test4b = (show $ resolve (abs "n" (var "m")) (var "q"))     == "FreeVar \"m\""
