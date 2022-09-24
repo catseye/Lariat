b174bc0c59b9a7f6beebd4fd51a4a2527348c8e7
Chris Pressey <cpressey@catseye.tc>
2022-09-24 17:52:49 +0100

    Test with ghc, or hugs, or both, depending on what's available.

--- test.sh
+++ test.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
-(cd impl/Haskell && ghc Main.hs -e main) || exit 1
-
+MISSING=""
+if command -v ghc > /dev/null 2>&1; then
+    echo "testing with ghc"
+    (cd impl/Haskell && ghc Main.hs -e main) || exit 1
+else
+    MISSING="${MISSING}G"
+fi
+if command -v runhugs > /dev/null 2>&1; then
+    echo "testing with hugs"
+    (cd impl/Haskell && runhugs Main.hs) || exit 1
+else
+    MISSING="${MISSING}H"
+fi
+if [ "x${MISSING}" = "xGH" ]; then
+    echo "Neither ghci nor runhugs found on executable search path. Aborting."
+    exit 1
+fi
